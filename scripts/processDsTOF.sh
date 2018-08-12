28768d6467345d68331750c48b0e1a571a6c080e
Linda Cremonesi <l.cremonesi@ucl.ac.uk>
2018-08-12 11:54:10 +0100

    Update script to clean up after producing rootfile

--- scripts/processDsTOF.sh
+++ scripts/processDsTOF.sh
@@ -17,35 +17,58 @@ do
 	continue
     fi
 
-    if [ "$runNumber" -lt 26 ]; then
+    if [ "$runNumber" -lt 2 ]; then
 	continue
     fi
     
-    
-    echo "Check if parsed files exist:"
-    if  ls $DATADIR/run${runNumber}/parsed* 1> /dev/null 2>&1; then
-	echo "Parsed files found!"
-    else
-	echo "Parsed files not found"
-	cd /home/lindac/DUNE/TOF/hptpcTofVmeReadout/
-	./decodeRun.sh $runNumber
-	cd -
+
+    hitmap=$DATADIR/run$runNumber/Run${runNumber}_histos.root
+    niceTree=$DATADIR/run${runNumber}/DsTOFtreeRun${runNumber}_tdc2.root
+
+    if [ -f $hitmap ]; then
+	echo "Found Everything!"
+	echo "Cleaning up parsed files"
+	if  ls $DATADIR/run${runNumber}/parsed* 1> /dev/null 2>&1; then
+	    echo "Parsed files found!"
+	    for parsed in `$DATADIR/run${runNumber}/parsed*`; do
+		echo "Removing parsed file " $parsed
+		rm $parsed
+	    do
+	fi
+
+	continue
     fi
 
-    niceTree=$dir/DsTOFtree_tdc1.root
+
     if [ -f $niceTree ]; then
 	echo "Nice trees exist!"    
     else 
-	echo "Producing nice tree"
-	./makeNiceTree $runNumber
-    fi
+	echo "Producing nice tree .."
 
+	echo "Check if parsed files exist:"
+	if  ls $DATADIR/run${runNumber}/parsed* 1> /dev/null 2>&1; then
+	    echo "Parsed files found!"
+	else
+	    echo "Parsed files not found"
+	    cd /home/lindac/DUNE/TOF/hptpcTofVmeReadout/
+	    ./decodeRun.sh $runNumber
+	    cd -
+	fi
+
+	./makeNiceTree $runNumber
+	
+    fi
+    
+    echo "Producing map tree!"
     ./makeHitMap $runNumber
     cp $DATADIR/run$runNumber/Run${runNumber}_hitMap.png          $htmlPageDir/plots/
     cp $DATADIR/run$runNumber/Run${runNumber}_coincidenceMap.png  $htmlPageDir/plots/
+    
+    
     
 done
 
 echo "Last rootified run number is " $runNumber
 
 /home/lindac/DUNE/TOF/DsTOFanalysis/scripts/templateHtml.sh $runNumber > $htmlPageDir/dstofSummary.html
+
