ab0201de61228e4a24976ca5ac77d604125d258f
Linda Cremonesi <l.cremonesi@ucl.ac.uk>
2018-09-03 17:22:42 +0100

    add functionalities to program

--- makeQuickTofPlot.cxx
+++ makeQuickTofPlot.cxx
@@ -29,37 +29,46 @@ int main(int argc, char *argv[]){
   
   string baseDir;
   string plotname;
+  string whereToSave;
   UInt_t startTime;
   UInt_t endTime;
   
-  if ((argc!=4)|| (argc!=5)){
-    std::cerr << "Usage: " << argv[0] << " [start Timestamp] [end Timestamp] [plot name] [optional baseDir]" << std::endl;
+  if ( (argc!=5) && (argc!=6)){
+    std::cerr << "Usage: " << argv[0] << " [start Timestamp] [end Timestamp] [plot name] [whereToSaveStuff] [optional baseDir]" << std::endl;
     return 1;
   } else {
     startTime = atoi(argv[1]);
     endTime = atoi(argv[2]);
     plotname += argv[3];
-    if (argc==5) baseDir += argv[4];
+    whereToSave += argv[4];
+    if (argc==6) baseDir += argv[5];
     else  baseDir +=  whereIsMyTOFdata;
+  }
+
+  if (startTime>endTime){
+    cout << "Start time is greater then end time, check your timestamps! " << endl;
+    return -1;
   }
 
 
   Int_t runMin=-1;
   Int_t runMax=-1;
   
-  for (int irun=850; irun<1090; irun++){
+
+  for (int irun=1000; irun<1090; irun++){
     
     TFile *fin = new TFile(Form("%s/run%d/DsTOFcoincidenceRun%d_tdc1.root", baseDir.c_str(), irun, irun), "read");
-    RawDsTofCoincidence *tofCoin = NULL;
+    RawDsTofCoincidence *tofCoinTemp = NULL;
     TTree *tree = (TTree*) fin->Get("tofCoinTree");
-    tree->SetBranchAddress("tofCoin", &tofCoin);
+    tree->SetBranchAddress("tofCoin", &tofCoinTemp);
     tree->GetEntry(0);
-    UInt_t firstTemp = tofCoin->unixTime[0];
+    UInt_t firstTemp = tofCoinTemp->unixTime[0];
     tree->GetEntry(tree->GetEntries()-1);
-    UInt_t lastTemp = tofCoin->unixTime[0];
+    UInt_t lastTemp = tofCoinTemp->unixTime[0];
 
     fin->Close();
     delete fin;
+
     if (firstTemp>endTime){
       break;
     }
@@ -74,7 +83,54 @@ int main(int argc, char *argv[]){
 
   }
 
+  if (runMin==-1 || runMax==-1){
+    cout << " Couldn't start or end run, check your timestamps!! " << endl;
+    return -1;
+  }
+
   cout << "Min and max runs are " << runMin << " " << runMax << endl;
+
+  TH1D* hTof = new TH1D("hTof", "", 300, 0, 250);
+
+  for (int itdc=0; itdc<2; itdc++){
+    TChain *tofCoinChain = new TChain("tofCoinTree");
+
+    for (int irun=runMin; irun<runMax+1; irun++){
+      tofCoinChain->Add(Form("%s/run%d/DsTOFcoincidenceRun%d_tdc%d.root", baseDir.c_str(), irun, irun, itdc+1));
+    }
+
+    RawDsTofCoincidence *tofCoin = NULL;
+    tofCoinChain->SetBranchAddress("tofCoin", &tofCoin);
+
+    double dstofHitT, deltat;
+
+    for (int ientry=0; ientry<tofCoinChain->GetEntries(); ientry++){
+      tofCoinChain->GetEntry(ientry);
+      if (tofCoin->unixTime[0]<startTime) continue;
+      if (tofCoin->unixTime[0]>endTime) break;
+      
+      
+      deltat = TMath::Abs(tofCoin->fakeTimeNs[0]-tofCoin->fakeTimeNs[1]  );
+      dstofHitT = min(tofCoin->fakeTimeNs[0], tofCoin->fakeTimeNs[1]) - (10. - TMath::Abs(deltat) / 2 );
+      hTof->Fill(dstofHitT - tofCoin->usTofSignal);
+      
+    }
+  }
+  
+  TCanvas *c1 = new TCanvas("c1");
+  hTof->SetTitle(Form("%s: Time of flight, DsToF - UsToF; DsToF - UsToF / ns; Events", plotname.c_str()));
+  hTof->SetLineColor(kRed);
+  hTof->SetFillColor(kRed);
+  hTof->SetFillStyle(3005);
+  hTof->Draw("hist");
+  c1->Print(Form("%s/%s.png", whereToSave.c_str(), plotname.c_str()));
+  c1->Print(Form("%s/%s.pdf", whereToSave.c_str(), plotname.c_str()));
+
+  TFile *fout = new TFile (Form("%s/%s.root", whereToSave.c_str(), plotname.c_str()), "recreate");
+  hTof->Write(Form("htof_%s", plotname.c_str()));
+
+  fout->Close();
+
 
   return 0;
 }
