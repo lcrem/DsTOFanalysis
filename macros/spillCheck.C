cb0bd7b8e27c0ae9b22409d591d69a313f4a1c69
sebj101 <jonesseb100@gmail.com>
2018-11-26 12:51:14 +0000

    Outputs hits in unmatched spills

--- macros/spillCheck.C
+++ macros/spillCheck.C
@@ -2,8 +2,8 @@
 
 // Checks a if an umatched spill in a given time frame is empty
 void spillCheck (const int timeStart , const int timeEnd) {
-  TFile *usDB = new TFile("utofBeamSpills.root");
-  TFile *dsDB = new TFile("dtofBeamSpills.root");
+  TFile *usDB = new TFile("/unix/dune/sjones/utofBeamSpills.root");
+  TFile *dsDB = new TFile("/unix/dune/sjones/dtofBeamSpills.root");
   TTree *dsTree = (TTree*)dsDB->Get("beamTree");
   TTree *usTree = (TTree*)usDB->Get("utofBeamTree");
 
@@ -22,7 +22,7 @@ void spillCheck (const int timeStart , const int timeEnd) {
   usTree->SetBranchAddress("unixRunStart", &unixRunStartUs);
   usTree->SetBranchAddress("nsTime", &nsTimeUs);
 
-  vector< pair<double, int> > unmatchedVec;
+  std::vector< pair<double, int> > unmatchedVec;
   
   int totalSpills = 0;
   int unmatched = 0;
@@ -51,6 +51,7 @@ void spillCheck (const int timeStart , const int timeEnd) {
 	    lowestunixRunStartDs = unixRunStartDs;
 	    lowestfakeUnixUs = fakeUnixTimeUs;
 	    lowestfakeUnixDs = fakeUnixTimeDs;
+	    // Take the unix time of the unmatched ustof spill and subtract the start of the dstof run within which it is contained
 	    lowestNsDs = (fakeUnixTimeUs - unixRunStartDs) * 1e9;
 	    // Work out when the unmatched ustof signal is in the dstof file
 	    
@@ -68,9 +69,33 @@ void spillCheck (const int timeStart , const int timeEnd) {
   } // ustof spills
   double perc = (float(unmatched) / float(totalSpills)) * 100;
   std::cout<<unmatched<<" unmatched spills found out of "<<totalSpills<< " ("<<perc<<"%)"<<endl;
-
+  std::cout<<unmatchedVec.size()<<std::endl;
   // Now look up if there are spills in the dtof files
   for (int i=0; i<unmatchedVec.size(); i++) {
+    // Find and open file
+    for (int file = 1; file <= 1442; file++) {
+      if (file == unmatchedVec[i].second) {
+	TH1D *htof = new TH1D("htof", Form("Dstof hits in missing spill %d; Time / ns", i), 250, 0, 1e9);
+	TFile *dtoffile = new TFile(Form("/unix/dune/hptpctof/run%d/DsTOFcoincidenceRun%d_tdc1.root", file, file), "read");
+	TTree *dtofTree = (TTree*)dtoffile->Get("tofCoinTree");
+	RawDsTofCoincidence *tempcoin = NULL;
+	dtofTree->SetBranchAddress("tofCoin", &tempcoin);
 
+	// Plot the events that are in the missing spill
+	for (int t=0; t<dtofTree->GetEntries(); t++) {
+	  dtofTree->GetEntry(t);
+	  if (tempcoin->fakeTimeNs[0] >= unmatchedVec[i].first && tempcoin->fakeTimeNs[0] <= unmatchedVec[i].first + 1e9) {
+	    htof->Fill(tempcoin->fakeTimeNs[0] - unmatchedVec[i].first);
+	  }
+	}
+	TCanvas *c = new TCanvas(Form("c%d", i));
+	htof->Draw("hist");
+	c->Print(Form("unmatchedspill%d.png", i));
+	c->Print(Form("unmatchedspill%d.pdf", i));
+	delete tempcoin;
+	delete dtofTree;
+	delete dtoffile;
+      }
+    }
   }
 }
