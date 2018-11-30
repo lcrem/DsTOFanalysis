49f41d846a3674f7cfd91c22663414b3b177ded1
Seb Jones <jonesseb100@gmail.com>
2018-11-30 11:31:38 +0000

    Make spill matching function

--- makeSpillDB.cxx
+++ makeSpillDB.cxx
@@ -34,10 +34,13 @@ const char* dsDir = "/scratch0/dbrailsf/temp/mylinktodtof/";
 
 // Makes tree of all the ustof beam spill times
 TTree *ustofSpillTree(int i);
-
+// Builds vector of ustof file start/end times
 vector<pair<vector<double>, TString>> ustofFileVec();
-
-TString checkUstofFilesVec(double time, vector<pair<vector<double>, TString>> fileVec);
+// Checks all ustof files to see if they contain a spill at a given unix time
+TString checkUstofFiles(double time, vector<pair<vector<double>, TString>> fileVec);
+// For a given utof file and time returns utof spill time if there is a spill within a certain time frame
+// Returns -1 otherwise
+double findSpill(double time, TString fileName, double window = 3.);
 
 int main(int argc, char *argv[]) {
 
@@ -74,49 +77,101 @@ int main(int argc, char *argv[]) {
   spillTree->Branch("unixRunStartUs", &unixRunStartUs);
   spillTree->Branch("nsTimeUs", &nsTimeUs);
 
-  //  TTree *usFiles = ustofFileTree();
   vector<pair<vector<double>, TString>> usFiles = ustofFileVec();
   // Loop over all the dstof files
   for (unsigned int file = firstRun; file <= lastRun; file++) {
-
     const char* filename = Form("%srun%d/DsTOFcoincidenceRun%d_tdc1.root", dsDir, file, file);
     if (!gSystem->AccessPathName(filename)) {
       cout<<"Opening run "<<file<<endl;
-      TFile *inFile = new TFile(Form("%srun%d/DsTOFcoincidenceRun%d_tdc1.root", dsDir, file, file), "read");
-      TTree *coinTree = (TTree*)inFile->Get("tofCoinTree");
-      RawDsTofCoincidence *tempcoin = NULL;
-      coinTree->SetBranchAddress("tofCoin", &tempcoin);
-      coinTree->GetEntry(0);
-      int runStart = tempcoin->unixTime[0];
+      TFile *inFile1 = new TFile(Form("%srun%d/DsTOFcoincidenceRun%d_tdc1.root", dsDir, file, file), "read");
+      TFile *inFile2 = new TFile(Form("%srun%d/DsTOFcoincidenceRun%d_tdc2.root", dsDir, file, file), "read");
+      TTree *coinTree1 = (TTree*)inFile1->Get("tofCoinTree");
+      TTree *coinTree2 = (TTree*)inFile2->Get("tofCoinTree");
+      RawDsTofCoincidence *tempcoin1 = NULL;
+      RawDsTofCoincidence *tempcoin2 = NULL;
+      coinTree1->SetBranchAddress("tofCoin", &tempcoin1);
+      coinTree2->SetBranchAddress("tofCoin", &tempcoin2);
+      coinTree1->GetEntry(0);
+      int runStart = tempcoin1->unixTime[0];
       cout<<"Start: "<<runStart<<endl;
       // Find first spill in the file
       double lastdelayed = 0.;
       double firstSpillNs   = 0.;
       double firstSpillUnix = 0.;
-      for (int t = 0; t < coinTree->GetEntries(); t++) {
-	coinTree->GetEntry(t);
-	if (tempcoin->lastDelayedBeamSignal > 0. && tempcoin->lastDelayedBeamSignal - tempcoin->lastRawBeamSignal > 0.5e9 && tempcoin->lastDelayedBeamSignal - tempcoin->lastRawBeamSignal < 1e9) {
-	  firstSpillNs = tempcoin->lastDelayedBeamSignal;
-	  firstSpillUnix = (tempcoin->lastDelayedBeamSignal / 1e9) + runStart;
+      TString goodFile;
+      for (int t = 0; t < coinTree1->GetEntries(); t++) {
+	coinTree1->GetEntry(t);
+	if (tempcoin1->lastDelayedBeamSignal > 0. && tempcoin1->lastDelayedBeamSignal - tempcoin1->lastRawBeamSignal > 0.5e9 && tempcoin1->lastDelayedBeamSignal - tempcoin1->lastRawBeamSignal < 1e9) {
+	  firstSpillNs = tempcoin1->lastDelayedBeamSignal;
+	  firstSpillUnix = (tempcoin1->lastDelayedBeamSignal / 1e9) + runStart;
 	  cout.precision(17);
 	  cout<<firstSpillUnix<<endl;
 	  // Now search through ustof files to find in which one this lies
-	  TString goodFile = checkUstofFilesVec(firstSpillUnix, usFiles);
-	  if (goodFile != "nofile") {
-	    cout<<goodFile<<endl;
-	  }
+	  goodFile = checkUstofFiles(firstSpillUnix, usFiles); 
+	  cout<<goodFile<<endl;
 	  break;
 	}
       } // Loop over entries
-            
-      delete coinTree;
-      delete inFile;
+      // Try and find matching spill within the ustof file
+      if (goodFile != "nofile") {
+	double spill = findSpill(firstSpillUnix, goodFile);
+      }
+
+      // Find entry with tSoSd within 3s of given spill
+      delete tempcoin1;
+      delete coinTree1;
+      delete inFile1;
+      delete tempcoin2;
+      delete coinTree2;
+      delete inFile2;
     }
 
   } // Loop over dstof files
 } // main
 
-TString checkUstofFilesVec(double time, vector<pair<vector<double>, TString> > fileVec) {
+double findSpill(double time, TString fileName, double window) {
+  double spillT = -1.;
+  cout<<"Looking for matching spill in ustof file "<<fileName<<endl;
+  // Open file
+  TString path = fileName;
+  path.Prepend(usDir);
+  TFile *ustofFile = new TFile(path, "read");
+  TTree *tree = (TTree*)ustofFile->Get("tree");
+  TNamed *start = 0;
+  ustofFile->GetObject("start_of_run", start);
+  const char* startchar = start->GetTitle();
+  string startstr(startchar);
+  string unixstart = startstr.substr(25,10);
+  double ustofStart = stod(unixstart);	 
+  // Branch vars
+  double tSoSd;
+  double tToF[50];
+  int nhit;
+  tree->SetBranchAddress("tSoSd", &tSoSd);
+  tree->SetBranchAddress("tToF", tToF);
+  tree->SetBranchAddress("nhit", &nhit);
+  double lastUnix = 0.;
+  int nMatch = 0;
+  // Loop over entries
+  for (int i=0; i<tree->GetEntries(); i++) {
+    tree->GetEntry(i);
+    // Find tSoSd that match within window
+    double ustofUnix = ustofStart + (tSoSd / 1e9);
+    if (ustofUnix >= (time - window) && ustofUnix <= (time + window) && ustofUnix != lastUnix) {
+      cout<<"Spill at "<<ustofUnix<<endl;
+      spillT = ustofUnix;
+      nMatch++;
+      lastUnix = ustofUnix;
+    }
+  }
+  cout<<nMatch<<" matching spills"<<"\n"<<endl;
+  delete tree;
+  delete ustofFile;
+
+  return spillT;
+}
+
+TString checkUstofFiles(double time, vector<pair<vector<double>, TString> > fileVec) {
   TString ustofFile = "nofile";
   for (int i=0; i<fileVec.size(); i++) {
     if (time >= fileVec[i].first[0] && time <= fileVec[i].first[1]) {
