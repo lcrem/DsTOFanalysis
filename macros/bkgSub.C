c2dcc9d3bce85faa615c15e34aa2e900304e9e20
Seb Jones <jonesseb100@gmail.com>
2019-02-05 17:19:39 +0000

    Started script to do background subtraction

---
+++ macros/bkgSub.C
@@ -0,0 +1,13 @@
+// bkgSub.C
+// Performs background subtraction on the dtof spectrum
+
+void bkgSub (const char* saveDir, 
+	     const int firstBlock,
+	     const int lastBlock,
+	     const char* dstofDir="/scratch0/dbrailsf/temp/mylinktodtof/") 
+{
+
+
+} // bkgSub
+
+
