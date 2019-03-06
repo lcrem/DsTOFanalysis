176eaddfe9d28ea1564465f3202cc465deff0f9d
Seb Jones <jonesseb100@gmail.com>
2019-03-06 15:50:24 +0000

    Count utof s1s2

--- macros/deadtime.C
+++ macros/deadtime.C
@@ -34,24 +34,132 @@ void deadtime(const char* saveDir,
   const double start4Block = 1535836129;
   const double end4Block   = 1535879634;
 
-  
   for (int nBlocks = 0; nBlocks <=4; nBlocks++) {
-    // Open files by hand since we're doing this locally
+    Int_t runMin=-1;
+    Int_t runMax=-1;
+
+    double startTime = 0;
+    double endTime   = 0;
+
+    const char* nustof;
     if (nBlocks == 0) {
-      
+      startTime = start0Block;
+      endTime   = end0Block;
+      nustof = Form("%s/%s", ustofDir, str0Block);
     }
     else if (nBlocks == 1) {
-
+      startTime = start1Block;
+      endTime   = end1Block;
+      nustof = Form("%s/%s", ustofDir, str1Block);
     }
     else if (nBlocks == 2) {
-
+      startTime = start2Block;
+      endTime   = end2Block;
+      nustof = Form("%s/%s", ustofDir, str2Block);
     }
     else if (nBlocks == 3) {
-
+      startTime = start3Block;
+      endTime   = end3Block;
+      nustof = Form("%s/%s", ustofDir, str3Block);
     }
     else if (nBlocks == 4) {
-
+      startTime = start4Block;
+      endTime   = end4Block;
+      nustof = Form("%s/%s", ustofDir, str4Block);
     }
+
+    // Go through utof files and count the number of hits
+    TFile *futof = new TFile(nustof, "read");
+
+    double tToF[50];
+    double tTrig;
+    double tS1;
+    double tSoSd;
+    int nhit;
+    int nBar[50];
+
+    TTree *tree = (TTree*)futof->Get("tree");
+
+    tree->SetBranchAddress("nhit", &nhit);
+    tree->SetBranchAddress("tS1", &tS1);
+    tree->SetBranchAddress("tToF", tToF);
+    tree->SetBranchAddress("tTrig", &tTrig);
+    tree->SetBranchAddress("tSoSd", &tSoSd);
+    tree->SetBranchAddress("nBar", nBar);
+
+    int nS1S2 = 0;
+    // Count number of S1 x S2 hits
+    for (int t=0; t<tree->GetEntries(); t++ ) {
+      tree->GetEntry(t);
+      if (tTrig !=0) {
+	nS1S2++;
+      }
+    } // for (int t=0; t<tree->GetEntries(); t++ )
+
+    // Find the correct dtof files
+    for (int irun=1000; irun<1100; irun++) {
+      TFile *fin = new TFile(Form("%srun%d/DsTOFcoincidenceRun%d_tdc1.root", dstofDir, irun, irun), "read");
+      RawDsTofCoincidence *tofCoinTemp = NULL;
+      TTree *tree = (TTree*) fin->Get("tofCoinTree");
+      tree->SetBranchAddress("tofCoin", &tofCoinTemp);
+      tree->GetEntry(0);
+      UInt_t firstTemp = tofCoinTemp->unixTime[0];
+      tree->GetEntry(tree->GetEntries()-1);
+      UInt_t lastTemp = tofCoinTemp->unixTime[0];
+      
+      fin->Close();
+      delete fin;
+      
+      if (firstTemp>endTime){
+	break;
+      }
+      
+      if (firstTemp<startTime && lastTemp>startTime){
+	runMin = irun;
+      }
+      
+      if (firstTemp<endTime && lastTemp>endTime){
+	runMax = irun;
+      }   
+    } // for (int irun=950; irun<1400; irun++) 
+    
+    cout << "Min and max runs are " << runMin << " " << runMax << endl;
+
+    // Now go over dtof files and count the number of s1s2 coincidences
+    for (int irun = runMin; irun < runMax+1; irun++) {
+      double tempUstof;
+      double ustofNs;
+      // Utof and beam signals go into both TDCs so only need to run over one
+      TFile *tofFile = new TFile(Form("%srun%d/DsTOFtreeRun%d_tdc1.root", dstofDir, irun, irun));
+      RawDsTofHeader *tof = NULL;
+      TTree *ustofTree = new TTree("ustofTree", "ustof");
+      ustofTree->SetDirectory(0);
+      ustofTree->Branch("ustofNs", &ustofNs, "ustofNs/D");
+      tempUstof=0;
+      double tempBeam;
+      double beamNs;
+      TTree *beamTree = new TTree("beamTree", "beam");
+      beamTree->SetDirectory(0);
+      beamTree->Branch("beamNs", &beamNs, "beamNs/D");
+      for (int i=0; i<tofTree->GetEntries(); i++) {
+	tofTree->GetEntry(i);
+	if (tof->unixTime < startTime) continue;
+	if (tof->unixTime > endTime) break;
+	
+	if (tof->channel == 13) {
+	  ustofNs = tof->fakeTimeNs - ustofDelay;
+	  // ustof signals shouldn't be coming closer than 500ns
+	  if ( (ustofNs - tempUstof) < 500.) continue;
+	  ustofTree->Fill();
+	  tempUstof = ustofNs;
+	} // if (tof->channel == 13) 
+	else if (tof->channel == 14) {
+
+	}
+      } // for (int i=0; i<tof->GetEntries(); i++)
+    }
+
   } // for (int nBlocks = 0; nBlocks <=4; nBlocks++)
+   
   
 } // deadtime
