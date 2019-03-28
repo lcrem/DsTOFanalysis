c679a308ac07514cd61251ec015f5776e7ceee19
Seb Jones <jonesseb100@gmail.com>
2019-03-28 11:55:25 +0000

    Change to ns for all calculations

--- macros/s4MomCalc.C
+++ macros/s4MomCalc.C
@@ -68,7 +68,7 @@ void s4MomCalc(const char* saveDir,
   THStack *hsMom = new THStack("hsMom", "Proton momentum as measured in S4; Proton momentum [GeV/c]; Events / spill");
   TLegend *leg = new TLegend(0.15, 0.5, 0.45, 0.88);
   
-  TFile *fout = new TFile(Form("%s/s4MomCalcPlots_eff.root", saveDir), "recreate");
+  TFile *fout = new TFile(Form("%s/s4MomCalcPlots.root", saveDir), "recreate");
 
   for (int nBlocks = 0; nBlocks <= 4; nBlocks++) {
     cout<<nBlocks<<" block case"<<endl;
@@ -200,10 +200,11 @@ void s4MomCalc(const char* saveDir,
 	spillTree->GetEntry(db);
 	if (globalSpillTime < startTime) continue;
 	if (globalSpillTime > endTime) break;
-	dstofSpillTimes.push_back(globalSpillTime - dstofStart);
-	ustofSpillTimes.push_back(ustofSpillTime - ustofStart);
+	dstofSpillTimes.push_back((globalSpillTime - dstofStart) * 1e9);
+	ustofSpillTimes.push_back((ustofSpillTime - ustofStart) * 1e9);
       } // for (int db=0; db<spillTree->GetEntries(); db++)
       cout<<ustofSpillTimes.size()<<" spills in run "<<irun<<endl;
+      
       // For each matched spill make a vector of all the hit times in each system
       //for (int spill = 0; spill < utofSpillTimes.size(); spill++) {
 	// Calculate time drift for this spill by taking the 5 spills either side of it
@@ -211,7 +212,10 @@ void s4MomCalc(const char* saveDir,
 
       // For second spill in the run make the vector of hits
       if (ustofSpillTimes.size() > 10) {
-      TGraph *grTmp = new TGraph();
+	TH1D *s3s4Tof = new TH1D(Form("s3s4TofRun%d_%dblocks", irun, nBlocks), Form("S3 - S4 ToF, run %d, %d blocks", irun, nBlocks), 400, -1000, 1000);
+	TGraph *grs3s4= new TGraph();
+	grs3s4->SetTitle(Form("S3 - S4 vs. S3, run %d, %d blocks", irun, nBlocks));
+	TGraph *grTmp = new TGraph();
 	for (int i=0; i<10; i++) {
 	  grTmp->SetPoint(grTmp->GetN(), ustofSpillTimes[i], ustofSpillTimes[i] - dstofSpillTimes[i]);
 	} // for (int i=0; i<ustofSpillTimes; i++) 
@@ -219,7 +223,7 @@ void s4MomCalc(const char* saveDir,
 	TF1 *f1 = new TF1("f1", "pol1");
 	grTmp->Fit(f1);
 	double grad = f1->GetParameter(1);
-	cout<<"Time drift is "<<grad<<" seconds / seconds"<<endl;
+	cout<<"Time drift is "<<grad<<" ns / ns"<<endl;
 
 	// Get dstof hits in the spill in vectors
 	vector<double> dstofHits1;
@@ -229,33 +233,71 @@ void s4MomCalc(const char* saveDir,
 	
 	for (int d = 0; d < dstofTree1->GetEntries(); d++) {
 	  dstofTree1->GetEntry(d);
-	  if ((tofCoin1->fakeTimeNs[0]/1e9 - dstofSpillTimes[1]) < 0.) continue;
-	  if ((tofCoin1->fakeTimeNs[0]/1e9 - dstofSpillTimes[1]) > 1.) break;
+	  if ((tofCoin1->fakeTimeNs[0] - dstofSpillTimes[1]) < 0.) continue;
+	  if ((tofCoin1->fakeTimeNs[0] - dstofSpillTimes[1]) > 1e9) break;
 	  double deltat = TMath::Abs(tofCoin1->fakeTimeNs[0]-tofCoin1->fakeTimeNs[1]);
 	  double dstofHitT = min(tofCoin1->fakeTimeNs[0], tofCoin1->fakeTimeNs[1]) - (10. - TMath::Abs(deltat) / 2.);
-	  dstofHits1.push_back(dstofHitT/1e9 - dstofSpillTimes[1]);
+	  dstofHits1.push_back(dstofHitT - dstofSpillTimes[1]);
 	} // for (int d = 0; d < dstofTree1->GetEntries(); d++)
 	for (int d = 0; d < dstofTree2->GetEntries(); d++) {
 	  dstofTree2->GetEntry(d);
-	  if ((tofCoin2->fakeTimeNs[0]/1e9 - dstofSpillTimes[1]) < 0.) continue;
-	  if ((tofCoin2->fakeTimeNs[0]/1e9 - dstofSpillTimes[1]) > 1.) break;
+	  if ((tofCoin2->fakeTimeNs[0] - dstofSpillTimes[1]) < 0.) continue;
+	  if ((tofCoin2->fakeTimeNs[0] - dstofSpillTimes[1]) > 1e9) break;
 	  double deltat = TMath::Abs(tofCoin2->fakeTimeNs[0]-tofCoin2->fakeTimeNs[1]);
 	  double dstofHitT = min(tofCoin2->fakeTimeNs[0], tofCoin2->fakeTimeNs[1]) - (10. - TMath::Abs(deltat) / 2.);
-	  dstofHits2.push_back(dstofHitT/1e9 - dstofSpillTimes[1]);
+	  dstofHits2.push_back(dstofHitT - dstofSpillTimes[1]);
 	}
 	// Get ustof hits in a vector (accounting for time drift)
 	for (int u = 0; u < tree->GetEntries(); u++) {
 	  tree->GetEntry(u);
-	  if ((tS1/1e9 - ustofSpillTimes[1]) < 0.) continue;
-	  if ((tS1/1e9 - ustofSpillTimes[1]) > 1.) break;
+	  if ((tS1 - ustofSpillTimes[1]) < 0.) continue;
+	  if ((tS1 - ustofSpillTimes[1]) > 1e9) break;
 	  for (int n = 0; n < nhit; n++) {
-	    ustofHits.push_back((tToF[n]/1e9 - ustofSpillTimes[1]) / (1. - grad));
+	    ustofHits.push_back((tToF[n] - ustofSpillTimes[1]) / (1. - grad));
 	  } // for(int n = 0; n < nhit; n++) 
 	} // for (int u = 0; u < tree->GetEntries(); u++)
 
-	  cout<<"Have counted "<<dstofHits1.size()+dstofHits2.size()<<" dstof hits and "<<ustofHits.size()<<" ustof hits"<<endl;
+	cout<<"Have counted "<<dstofHits1.size()+dstofHits2.size()<<" dstof hits and "<<ustofHits.size()<<" ustof hits"<<endl;
+	// Now we want to try and match these hits together
+	// Fewer S3 hits per spill in general 
+	// Try and match each of these with the corresponding closest S4 hit
+	int lastdh1 = 0;
+	int lastdh2 = 0;
+	for (int uh = 0; uh < ustofHits.size(); uh++) {
+	  if (uh % 100 == 1) {
+	    cout<<"Ustof event no. "<<uh<<endl;
+	  }
+	  double diffLow = 999999999999.;
+	  double diff = 0.;
+	  int hitLow = -1;
+	  int tdc = 0;
+	  for (int dh1=lastdh1; dh1<dstofHits1.size(); dh1++) {
+	    diff = ustofHits[uh] - dstofHits1[dh1];
+	    if (abs(diff) < abs(diffLow)) {
+	      diffLow = diff;
+	      hitLow = dh1;
+	      tdc = 1;
+	      lastdh1 = dh1;
+	    }
+	    //	    else { break; }
+	  } // for (int dh=0; dh<dstofHits1.size(); dh++) 
+	  for (int dh2=lastdh2; dh2<dstofHits2.size(); dh2++) {
+	    diff = ustofHits[uh] - dstofHits1[dh2];
+	    if (abs(diff) < abs(diffLow)) {
+	      diffLow = diff;
+	      hitLow = dh2;
+	      tdc = 2;
+	      lastdh2 = dh2;
+	    }
+	    //	    else { break; }
+	  } // for (int dh=0; dh<dstofHits2.size(); dh++)
+	  s3s4Tof->Fill(diffLow);
+	  grs3s4->SetPoint(grs3s4->GetN(), ustofHits[uh], diffLow);
+	} // for (int uh = 0; uh < ustofHits.size(); uh++)
+	fout->cd();
+	grs3s4->Write(Form("grs3s4Run%d_%dblocks", irun,nBlocks)); 
+	s3s4Tof->Write();
       } // if (ustofSpillTimes.size() > 10) 
-
 
       dbFile->Close();
       delete tofCoin1; 
@@ -268,4 +310,5 @@ void s4MomCalc(const char* saveDir,
     ustofIn->Close();
   } // for (int nBlocks = 0; nBlocks <= 4; nBlocks++) 
 
+  fout->Close();
 } // s4MomCalc
