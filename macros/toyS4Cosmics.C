7be368456216ff742a29d97a24bbf9c6a0ba3834
Seb Jones <jonesseb100@gmail.com>
2019-10-15 20:14:50 +0100

    Finally got it working with a 3D model

--- macros/toyS4Cosmics.C
+++ macros/toyS4Cosmics.C
@@ -1,22 +1,27 @@
 // toyS4Cosmics.C
 // Macro to estimate the number of coincidences we should see in each bar
 
-const double cosmicStartY = 10.;
-const double cosmicLowX = -25.;
-const double cosmicHiX  = 25.;
+const double startRadius  = 5.;
 
-double yAtx(const double xStart, const double x, const double theta, const double yStart = cosmicStartY) 
+TVector3 intersectionPnt(const TVector3 startPnt, const TVector3 dir, 
+			 const TVector3 barVec, const TVector3 barPnt)
 {
-  double y = (1./TMath::Tan(theta)) * (x - xStart) + yStart; 
-  return y;
+  double d = ((barPnt - startPnt).Dot(barVec))/dir.Dot(barVec);
+  TVector3 intersection = d * dir + startPnt;
+  return intersection;
 }
 
-bool crossesBar(const double barX, const double barY, const double xStart, const double theta) 
+bool crossesBar(const TVector3 startPnt, const TVector3 dir, const TVector3 barVec,  
+		const TVector3 bar1, const TVector3 bar2, const TVector3 bar3)
 {
-  double y1 = yAtx(xStart, barX, theta);
-  double y2 = yAtx(xStart, barX-0.01, theta); // Because bars have finite width
-  bool crosses = ((y1 < barY && y1 > barY - 0.1) || (y2 < barY && y2 > barY - 0.1));
-  return crosses;
+  TVector3 intPnt = intersectionPnt(startPnt, dir, barVec, bar1);
+  bool cross = false;
+  if ((bar2 * (bar1-bar2)) <= (intPnt * (bar1-bar2)) && 
+      (bar1 * (bar1-bar2)) >= (intPnt * (bar1-bar2)) &&
+      (bar2 * (bar3-bar2)) <= (intPnt * (bar3-bar2)) && 
+      (bar3 * (bar3-bar2)) >= (intPnt * (bar3-bar2)))
+    cross = true;
+  return cross;
 }
 
 void toyS4Cosmics(const char* saveDir, const int nAttempts = 1e8)
@@ -52,56 +57,79 @@ void toyS4Cosmics(const char* saveDir, const int nAttempts = 1e8)
   // Bar 1
   const TVector3 D5_D5L(-0.3797, -0.2530, 12.4000);
   const TVector3 D5_D5R(-1.1521, -0.2506, 12.3576);
-  const std::vector<TVector3> S4BarsL = {D5_D5L, D5_U5L, D5_D4L, D5_U4L, D5_D3L, D5_U3L, 
+  const std::vector<TVector3> s4BarsL = {D5_D5L, D5_U5L, D5_D4L, D5_U4L, D5_D3L, D5_U3L, 
 					 D5_D2L, D5_U2L, D5_D1L, D5_U1L};
-  const std::vector<TVector3> S4BarsR = {D5_D5R, D5_U5R, D5_D4R, D5_U4R, D5_D3R, D5_U3R, 
+  const std::vector<TVector3> s4BarsR = {D5_D5R, D5_U5R, D5_D4R, D5_U4R, D5_D3R, D5_U3R, 
 					 D5_D2R, D5_U2R, D5_D1R, D5_U1R};
 
   const double yOffset = 0.3530;
   double avgX = 0.;
-  for (int i=0; i<S4BarsL.size(); i++) {
-    avgX += S4BarsL.at(i).Z();
+  double avgZ = 0.;
+  for (int i=0; i<s4BarsL.size(); i++) {
+    avgX += s4BarsL.at(i).X();
+    avgX += s4BarsR.at(i).X();
+    avgZ += s4BarsL.at(i).Z();
+    avgZ += s4BarsR.at(i).Z();
   }
-  avgX /= S4BarsL.size();
-  // Because we are doing this in 2D for now, we can imagine that we are in a plane 
-  // which is directly end on to the bars
-  std::vector<double> s4BarTops;
-  std::vector<double> s4BarBottoms;
-  std::vector<double> s4BarXs;
-  for (int i=0; i<S4BarsL.size(); i++) {
-    s4BarTops.push_back(S4BarsL.at(i).Y() + yOffset);
-    s4BarBottoms.push_back(S4BarsL.at(i).Y() - 0.1 + yOffset);
-    s4BarXs.push_back(S4BarsL.at(i).Z() - avgX);
+  avgX /= (s4BarsL.size()+s4BarsR.size());
+  avgZ /= (s4BarsL.size()+s4BarsR.size());
+  cout<<"Avg x, z "<<avgX<<", "<<avgZ<<endl;
+  vector<double> s4BarXs;
+  vector<double> s4BarTops;
+  vector<double> s4BarBottoms;
+  TVector3 shiftVector(avgX, 0., avgZ);
+
+  vector<TVector3> s4LU, s4RU, s4LD, s4RD;
+  vector<TVector3> s4BarVec;
+  for (int i=0; i<s4BarsL.size(); i++) {
+    s4BarTops.push_back(s4BarsL.at(i).Y() + yOffset);
+    s4BarBottoms.push_back(s4BarsL.at(i).Y() - 0.1 + yOffset);
+    s4BarXs.push_back(s4BarsL.at(i).Z() - avgZ);
+    TVector3 LU, RU, LD, RD; 
+    LU = s4BarsL.at(i);
+    LU.SetY(LU.Y()+yOffset);
+    RU = s4BarsR.at(i);
+    RU.SetY(RU.Y()+yOffset);
+    LD = s4BarsL.at(i);
+    LD.SetY(LD.Y()+yOffset-0.1);
+    RD = s4BarsR.at(i);
+    RD.SetY(RD.Y()+yOffset-0.1);
+    s4LU.push_back(LU);
+    s4RU.push_back(RU);
+    s4LD.push_back(LD);
+    s4RD.push_back(RD);
+    s4BarVec.push_back(((LU - RU).Cross(RD - RU)).Unit());
   }
+  vector< vector<TVector3> > s4Pnts;
+  s4Pnts.push_back(s4LU);
+  s4Pnts.push_back(s4RU);
+  s4Pnts.push_back(s4LD);
+  s4Pnts.push_back(s4RD);
+
   TRandom3 *rand = new TRandom3(6789);
 
   TFile *fout = new TFile(saveDir, "recreate");
   // 2D approximation - x is such that the bars are 1d in x, y is vertical
-  // Cosmics are considered to be distributed uniformly in x and all start at same height
+  // Cosmics are considered to be distributed uniformly in spatial phi and theta
+  // However, need cos squared theta for direction
   TH1D *hCosDist = new TH1D("hCosDist", "Cosmic Distribution; #theta / #pi", 100., -0.5, 0.5);
   hCosDist->Sumw2();
   hCosDist->GetXaxis()->SetTitleSize(.05);
   hCosDist->GetYaxis()->SetTitleSize(.05);
   hCosDist->GetXaxis()->SetLabelSize(.05);
   hCosDist->GetYaxis()->SetLabelSize(.05);
-  TH1D *hCosXDist = new TH1D("hCosXDist", "Cosmic Distribution; X / m", 100., cosmicLowX, cosmicHiX);
-  hCosXDist->Sumw2();
-  hCosXDist->GetXaxis()->SetTitleSize(.05);
-  hCosXDist->GetYaxis()->SetTitleSize(.05);
-  hCosXDist->GetXaxis()->SetLabelSize(.05);
-  hCosXDist->GetYaxis()->SetLabelSize(.05);
-  TH2D *h2CosXAngDist = new TH2D("h2CosXAngDist", "Cosmic Distribution; X / m", 100., cosmicLowX, cosmicHiX, 100., -0.5, 0.5);
-  h2CosXAngDist->Sumw2();
-  h2CosXAngDist->GetXaxis()->SetTitleSize(.05);
-  h2CosXAngDist->GetYaxis()->SetTitleSize(.05);
-  h2CosXAngDist->GetXaxis()->SetLabelSize(.05);
-  h2CosXAngDist->GetYaxis()->SetLabelSize(.05);
-  TH1D *hCosEndX = new TH1D("hCosEndX", "Cosmic end point; X / m; Events", 100., cosmicLowX, cosmicHiX);
-  hCosEndX->Sumw2();
-  hCosEndX->GetXaxis()->SetTitleSize(.05);
-  hCosEndX->GetYaxis()->SetTitleSize(.05);
-  hCosEndX->GetXaxis()->SetLabelSize(.05);
-  hCosEndX->GetYaxis()->SetLabelSize(.05);
+  TH1D *hCosPhiDist = new TH1D("hCosPhiDist", "Cosmic Distribution; #phi / #pi", 100., 0, 2.);
+  hCosPhiDist->Sumw2();
+  hCosPhiDist->GetXaxis()->SetTitleSize(.05);
+  hCosPhiDist->GetYaxis()->SetTitleSize(.05);
+  hCosPhiDist->GetXaxis()->SetLabelSize(.05);
+  hCosPhiDist->GetYaxis()->SetLabelSize(.05);
+  TH2D *h2CosPhiThetaDist = new TH2D("h2CosPhiThetaDist", "Cosmic Distribution; #phi / #pi; #theta / #pi", 100., 0., 2., 100., -0.5, 0.5);
+  h2CosPhiThetaDist->Sumw2();
+  h2CosPhiThetaDist->GetXaxis()->SetTitleSize(.05);
+  h2CosPhiThetaDist->GetYaxis()->SetTitleSize(.05);
+  h2CosPhiThetaDist->GetXaxis()->SetLabelSize(.05);
+  h2CosPhiThetaDist->GetYaxis()->SetLabelSize(.05);
   TH1D *hBarHits = new TH1D("hBarHits", "Cosmic hits in each bar; Bar; Hits", 10, 0.5, 10.5);
   hBarHits->Sumw2();
   hBarHits->GetXaxis()->SetTitleSize(.05);
@@ -116,41 +144,100 @@ void toyS4Cosmics(const char* saveDir, const int nAttempts = 1e8)
   h2BarCoins->GetYaxis()->SetLabelSize(.05);
   h2BarCoins->GetZaxis()->SetTitleSize(.05);
   h2BarCoins->GetZaxis()->SetLabelSize(.05);
+  TH2D *h2EndPos = new TH2D("h2EndPos", "Cosmics end positions; X / m; Z / m; Number", 100, -8.+shiftVector.X(), 8.+shiftVector.X(), 100, -8.+shiftVector.Z(), 8.+shiftVector.Z());
+  h2EndPos->Sumw2();
+  h2EndPos->GetXaxis()->SetTitleSize(.05);
+  h2EndPos->GetYaxis()->SetTitleSize(.05);
+  h2EndPos->GetXaxis()->SetLabelSize(.05);
+  h2EndPos->GetYaxis()->SetLabelSize(.05);
+  h2EndPos->GetZaxis()->SetTitleSize(.05);
+  h2EndPos->GetZaxis()->SetLabelSize(.05);
+  TH3D *h3StartPos = new TH3D("h3StartPos", "Cosmic ray start positions; X / m; Y / m; Z / m", 50, -5+shiftVector.X(), 5+shiftVector.X(), 50, -5, 5, 50, -5+shiftVector.Z(), 5+shiftVector.Z());
+  h3StartPos->Sumw2();
+  h3StartPos->GetXaxis()->SetTitleSize(.05);
+  h3StartPos->GetYaxis()->SetTitleSize(.05);
+  h3StartPos->GetXaxis()->SetLabelSize(.05);
+  h3StartPos->GetYaxis()->SetLabelSize(.05);
+  h3StartPos->GetZaxis()->SetTitleSize(.05);
+  h3StartPos->GetZaxis()->SetLabelSize(.05);
+  TH3D *h3StartDir = new TH3D("h3StartDir", "Cosmic ray start directions; X / m; Y / m; Z / m", 50, -1, 1, 50, -1, 1, 50, -1, 1);
+  h3StartDir->Sumw2();
+  h3StartDir->GetXaxis()->SetTitleSize(.05);
+  h3StartDir->GetYaxis()->SetTitleSize(.05);
+  h3StartDir->GetXaxis()->SetLabelSize(.05);
+  h3StartDir->GetYaxis()->SetLabelSize(.05);
+  h3StartDir->GetZaxis()->SetTitleSize(.05);
+  h3StartDir->GetZaxis()->SetLabelSize(.05);
+  TH2D *h2StartXY = new TH2D("h2StartXY", "Cosmic ray start positions; X / m; Y / m", 50, -6.+shiftVector.X(), 6.+shiftVector.X(), 50, -6., 6.);
+  h2StartXY->Sumw2();
+  h2StartXY->GetXaxis()->SetTitleSize(.05);
+  h2StartXY->GetYaxis()->SetTitleSize(.05);
+  h2StartXY->GetXaxis()->SetLabelSize(.05);
+  h2StartXY->GetYaxis()->SetLabelSize(.05);
+  h2StartXY->GetZaxis()->SetTitleSize(.05);
+  h2StartXY->GetZaxis()->SetLabelSize(.05);
+  TH2D *h2StartXZ = new TH2D("h2StartXZ", "Cosmic ray start positions; X / m; Z / m", 50, -6+shiftVector.X(), 6.+shiftVector.X(), 25, -6.+shiftVector.Z(), 6+shiftVector.Z());
+  h2StartXZ->Sumw2();
+  h2StartXZ->GetXaxis()->SetTitleSize(.05);
+  h2StartXZ->GetYaxis()->SetTitleSize(.05);
+  h2StartXZ->GetXaxis()->SetLabelSize(.05);
+  h2StartXZ->GetYaxis()->SetLabelSize(.05);
+  h2StartXZ->GetZaxis()->SetTitleSize(.05);
+  h2StartXZ->GetZaxis()->SetLabelSize(.05);
 
+  vector<TH3D*> h3BarVec;
+  for (int b=0; b<10; b++) {
+    TH3D *h3Bar = new TH3D(Form("h3Bar%d", b+1), Form("Bar %d; X / m; Z / m; Y / m", b+1), 50, -1.+shiftVector.X(), 1.+shiftVector.X(), 50, -2, 2., 50, -.5+shiftVector.Z(), .5+shiftVector.Z());
+    h3BarVec.push_back(h3Bar);
+  }
+  TH3D *h3BarTot = new TH3D("h3BarTot", "Total; X / m; Z / m; Y / m", 50, -1.+shiftVector.X(), 1.+shiftVector.X(), 50, -2, 2., 50, -.5+shiftVector.Z(), .5+shiftVector.Z());
+
+  TVector3 floorVec(0., 1., 0.);
   for (int n = 0; n < nAttempts; n++) {
-    double flatpi = -TMath::Pi()/2. + rand->Rndm() * TMath::Pi();
+    double dirTheta = -TMath::Pi()/2. + rand->Rndm() * TMath::Pi();
     double flat = rand->Rndm();
-    if (flat < (TMath::Cos(flatpi) * TMath::Cos(flatpi))) {
-      hCosDist->Fill(flatpi/TMath::Pi());
-      double xStart = cosmicLowX + (cosmicHiX - cosmicLowX) * rand->Rndm();
-      hCosXDist->Fill(xStart);
-      h2CosXAngDist->Fill(xStart, flatpi/TMath::Pi());
-      hCosEndX->Fill(xStart + cosmicStartY * TMath::Tan(flatpi));
+    if (flat < (TMath::Cos(dirTheta) * TMath::Cos(dirTheta))) {
+      hCosDist->Fill(dirTheta/TMath::Pi());
+      double dirPhi = TMath::Pi() * rand->Rndm();
+      double spatialPhi   = -TMath::Pi() * rand->Rndm();
+      double spatialTheta = -TMath::Pi() + TMath::Pi() * rand->Rndm();
+      hCosPhiDist->Fill(dirPhi / TMath::Pi());
+      h2CosPhiThetaDist->Fill(dirPhi/TMath::Pi(), dirTheta/TMath::Pi());
+      TVector3 v;
+      v.SetMagThetaPhi(startRadius, spatialTheta, spatialPhi);
+      v += shiftVector;
+      h3StartPos->Fill(v.X(), v.Y(), v.Z());
+      h2StartXY->Fill(v.X(), v.Y());
+      h2StartXZ->Fill(v.X(), v.Z());
+      TVector3 dir;
+      dir.SetMagThetaPhi(1., dirPhi, dirTheta);
+      h3StartDir->Fill(-dir.X(), -dir.Y(), -dir.Z());
+      TVector3 f(0., 0., 0.);
+      TVector3 floorInt = intersectionPnt(v, -dir, floorVec, f);
+      h2EndPos->Fill(floorInt.X(), floorInt.Z());
       // Now test to see which bar (if any) it passes through
-      for (int b=0; b < s4BarXs.size(); b++) {
-	if (crossesBar(s4BarXs.at(b), s4BarTops.at(b), xStart, flatpi)) {
+      for (int b=0; b < s4Pnts[0].size(); b++) {	
+	TVector3 int1 = intersectionPnt(v, -dir, s4BarVec.at(b), s4Pnts.at(0).at(b));
+	if (crossesBar(v, -dir, s4BarVec.at(b), s4Pnts.at(1).at(b), 
+		       s4Pnts.at(0).at(b), s4Pnts.at(2).at(b)) && int1.Y() <= v.Y()) {
 	  hBarHits->Fill(b+1);
-	  for (int b2=b; b2 < s4BarXs.size(); b2++) {
-	    if (crossesBar(s4BarXs.at(b2), s4BarTops.at(b2), xStart, flatpi) && b2 != b) {
-	      // Determine which of these was struck first using x coord
-	      if (xStart < s4BarXs.at(b) && s4BarXs.at(b) < s4BarXs.at(b2))
-		h2BarCoins->Fill(b+1, b2+1);
-	      else if (xStart < s4BarXs.at(b2) && s4BarXs.at(b2) < s4BarXs.at(b))
-		h2BarCoins->Fill(b2+1, b+1);
-	      else if (xStart > s4BarXs.at(b) && s4BarXs.at(b) > s4BarXs.at(b2))
-		h2BarCoins->Fill(b+1, b2+1);  
-	      else if (xStart > s4BarXs.at(b2) && s4BarXs.at(b2) > s4BarXs.at(b))
-		h2BarCoins->Fill(b2+1, b+1);
-	      else
-		cout<<"You need to think about this more carefully"<<endl;
-	      
-	    } // Cosmic strikes a bar
-	  } // Loop over bars again
-	} // Cosmic strikes a bar
+	  for (int b2=b; b2 < s4Pnts[0].size(); b2++) {
+	    TVector3 int2 = intersectionPnt(v, -dir, s4BarVec.at(b2), s4Pnts.at(0).at(b2));
+	    if (crossesBar(v, -dir, s4BarVec.at(b2), s4Pnts.at(1).at(b2), 
+			   s4Pnts.at(0).at(b2), s4Pnts.at(2).at(b2)) && int2.Y() <= v.Y() && b!=b2) {
+	      if (int1.Y() > int2.Y()) h2BarCoins->Fill(b+1, b2+1);
+	      else h2BarCoins->Fill(b2+1, b+1);
+	      h3BarTot->Fill(int1.X(), int1.Z(), int1.Y());
+	      h3BarTot->Fill(int2.X(), int2.Z(), int2.Y());
+	      h3BarVec.at(b)->Fill(int1.X(), int1.Z(), int1.Y());
+	      h3BarVec.at(b2)->Fill(int2.X(), int2.Z(), int2.Y());
+	    }
+	  }
+	}
       } // Loop over bars
     }
   }
-  h2BarCoins->Scale(1. / h2BarCoins->Integral());
+  // h2BarCoins->Scale(1. / h2BarCoins->Integral());
 
   // Some graphs to help show bar positions
   TMultiGraph *mg = new TMultiGraph("mgBars", "S4 bar positions in MC; X / m; Y / m");
@@ -168,11 +255,20 @@ void toyS4Cosmics(const char* saveDir, const int nAttempts = 1e8)
   }
 
   fout->cd();
+  h3BarTot->Write();
+  for(int b=0; b<10; b++) {
+    h3BarVec.at(b)->Write();
+  }
   hBarHits->Write();
-  h2CosXAngDist->Write();
-  hCosXDist->Write();
+  h2CosPhiThetaDist->Write();
+  hCosPhiDist->Write();
   hCosDist->Write();
-  hCosEndX->Write();
+  // hCosEndX->Write();
+  h2EndPos->Write();
+  h3StartPos->Write();
+  h3StartDir->Write();
+  h2StartXY->Write();
+  h2StartXZ->Write();
   h2BarCoins->Write();
   mg->Write();
   fout->Close();
