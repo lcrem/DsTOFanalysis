5bca11f5ee75d7902cab39a5d152cf1499af201c
Seb Jones <jonesseb100@gmail.com>
2019-11-14 13:38:07 +0000

    Cut a load of stuff out

--- macros/fitGaussToCosmics.C
+++ macros/fitGaussToCosmics.C
@@ -8,10 +8,6 @@ void fitGaussToCosmics(const char* outFile, const char* s4Plots="~/work/hptpc/an
   TFile *fin = new TFile(s4Plots, "read");
   TH1D *hGausSigma = new TH1D("hGausSigma", "#sigma for bar efficiencies; #sigma / cm; Events", 10, 26, 36);
   TH1D *hGausMean = new TH1D("hGausMean", "#mu for bar efficiencies; #mu / cm; Events", 20, 50., 90.);
-
-  TH1D *hHalfGausSigma = new TH1D("hHalfGausSigma", "#sigma for bar efficiencies with half gaussians fitted; #sigma / cm; Events", 20, 4, 24);
-  TH1D *hHalfGausMean1 = new TH1D("hHalfGausMean1", "#mu_{1} for bar efficiencies with half gaussians fitted; #mu / cm; Events", 20, 20., 50.);
-  TH1D *hHalfGausMean2 = new TH1D("hHalfGausMean2", "#mu_{2} for bar efficiencies with half gaussians fitted; #mu / cm; Events", 20, 90., 120.); 
   // Loop over samples
   for (int sample=0; sample < 4; sample++) {
     cout<<"Sample "<<sample<<endl;
@@ -25,15 +21,6 @@ void fitGaussToCosmics(const char* outFile, const char* s4Plots="~/work/hptpc/an
       hCosmic->Fit(f1);
       hGausSigma->Fill(f1->GetParameter(2));
       hGausMean->Fill(f1->GetParameter(1));
-      // Same as this but fit some half Gaussians to the edges of the distributions
-      // TF1 *fHalf1 = new TF1("fHalf1", "gaus", 0, 40);
-      // TF1 *fHalf2 = new TF1("fHalf2", "gaus", 100, 140);
-      // hCosmic->Fit(fHalf1, "R");
-      // hCosmic->Fit(fHalf2, "R");
-      // hHalfGausSigma->Fill(fHalf1->GetParameter(2));
-      // hHalfGausMean1->Fill(fHalf1->GetParameter(1));
-      // hHalfGausSigma->Fill(fHalf2->GetParameter(2));
-      // hHalfGausMean2->Fill(fHalf2->GetParameter(1));
       fout->cd();
       hCosmic->Write();
       f1->Write(Form("fitBlock%dBar%d", sample, b));
@@ -43,9 +30,6 @@ void fitGaussToCosmics(const char* outFile, const char* s4Plots="~/work/hptpc/an
   fout->cd();
   hGausSigma->Write();
   hGausMean->Write();
-  hHalfGausSigma->Write();
-  hHalfGausMean1->Write();
-  hHalfGausMean2->Write();
   
   fin->Close();
   delete fin;
