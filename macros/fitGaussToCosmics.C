707a196ce2b6f90cfd0328893593d920e59046ad
Seb Jones <jonesseb100@gmail.com>
2019-11-27 15:22:59 +0000

    Nice comparisons for plots with different binnings

--- macros/fitGaussToCosmics.C
+++ macros/fitGaussToCosmics.C
@@ -6,45 +6,23 @@ void fitGaussToCosmics(const char* outFile, const char* s4Plots="~/work/hptpc/an
   gROOT->SetBatch(kTRUE);
   TFile *fout = new TFile(outFile, "recreate");
   TFile *fin = new TFile(s4Plots, "read");
-  TH1D *hGausSigma = new TH1D("hGausSigma", "#sigma for bar efficiencies; #sigma / cm; Events", 10, 26, 36);
-  TH1D *hGausMean = new TH1D("hGausMean", "#mu for bar efficiencies; #mu / cm; Events", 20, 50., 90.);
   // Loop over samples
   int nBad  = 0;
   int nStillBad  = 0;
   int nFits = 0;
   for (int sample=0; sample < 4; sample++) {
     cout<<"Sample "<<sample<<endl;
-    TH2D *h2Cosmics = (TH2D*)fin->Get(Form("h2CosmicsEffBins%d", sample));
+    TH2D *h2Cosmics     = (TH2D*)fin->Get(Form("h2CosmicsEff%d", sample));
+    TH2D *h2CosmicsBins = (TH2D*)fin->Get(Form("h2CosmicsEffBins%d", sample));
     for (int b=1; b<10; b++) {
       cout<<"Block "<<sample<<", bar "<<b<<endl;
-      cout<<"Gaussian to 4th power"<<endl;
-      TF1 *f1 = new TF1( Form("f1block%dbar%d", sample, b), "[0]*exp(-1*pow((x-[1])/(2*[2]), 4))" );
-      f1->SetParameter(0, 1);
-      f1->SetParameter(1, 70);
-      f1->SetParameter(2, 30);
-      TH1D *hCosmic = h2Cosmics->ProjectionX(Form("block%dbar%d", sample, b), b, b);
-      hCosmic->Fit(f1,"q");
-      hGausSigma->Fill(f1->GetParameter(2));
-      hGausMean->Fill(f1->GetParameter(1));
       fout->cd();
-      f1->Write();
-
-      cout<<"One sigmoid fitted"<<endl;
-      TF1 *ftmp = new TF1(Form("ftmpblock%dbar%d", sample, b), "([0]/(1+exp(-[1]*(x-[2]))))", 0, 40);
-      ftmp->SetParameter(0, 2);
-      ftmp->SetParameter(1, 0.15);
-      ftmp->SetParameter(2, 20);
-      hCosmic->Fit(ftmp, "q", "R");
-      ftmp->Write();
-      
-      cout<<"Gaussian to 6th power"<<endl;
-      TF1 *f3 = new TF1( Form("f3block%dbar%d", sample, b), "[0]*exp(-1*pow((x-[1])/(2*[2]), 6))" );
-      f3->SetParameter(0, 1);
-      f3->SetParameter(1, 70);
-      f3->SetParameter(2, 25);
-      hCosmic->Fit(f3, "q");
-      f3->Write();
-
+      TH1D *hCosmic     = h2Cosmics->ProjectionX(Form("block%dbar%d", sample, b), b, b);
+      TH1D *hCosmicBins = h2CosmicsBins->ProjectionX(Form("block%dbar%dbins", sample, b), b, b);
+      hCosmic->SetTitle(Form("%d blocks, bar %d; Efficiency; x / cm", sample, b));
+      hCosmicBins->SetTitle(Form("%d blocks, bar %d (finer bins); Efficiency; x / cm", sample, b));
+      hCosmic->SetLineWidth(2);
+      hCosmicBins->SetLineWidth(2);
       cout<<"Two sigmoids fitted"<<endl;
       // Fit the sigmoids individually  to one side then use the results to tune
       TF1 *f2_1 = new TF1(Form("f2_1block%dbar%d", sample, b), "[0]/(1+exp(-[1]*(x-[2])))", 0, 70);
@@ -66,6 +44,13 @@ void fitGaussToCosmics(const char* outFile, const char* s4Plots="~/work/hptpc/an
       f2->SetParameter(4, f2_2->GetParameter(2));
       TFitResultPtr r = hCosmic->Fit(f2);
       f2->Write();
+      TCanvas *c2 = new TCanvas(Form("c2_%d_%d", sample, b), Form("c2_%d_%d", sample, b), 1300, 500);
+      c2->Divide(2, 1);
+      c2->cd(1);
+      hCosmic->Draw();
+      TText *t1 = new TText(40, .15, Form("#Chi^{2} = %.4g/%d", f2->GetChisquare(), f2->GetNDF()));
+      t1->Draw();
+      //f2->Draw("same");
       hCosmic->Write();
       Int_t fitStatus = r;
       if (fitStatus != 0) {
@@ -86,14 +71,19 @@ void fitGaussToCosmics(const char* outFile, const char* s4Plots="~/work/hptpc/an
 	}
       }
       nFits++;
-    }
+      c2->cd(2);
+      hCosmicBins->Fit(f2);
+      hCosmicBins->Draw();
+      TText *t2 = new TText(40, .15, Form("#Chi^{2} = %.4g/%d", f2->GetChisquare(), f2->GetNDF()));
+      t2->Draw();
+      hCosmicBins->Write();
+      c2->Print(Form("~/work/hptpc/fitGaussToCosmics/compBlock%dbar%d.pdf", sample, b));
+      c2->Print(Form("~/work/hptpc/fitGaussToCosmics/compBlock%dbar%d.tex", sample, b));
+    } // Loop over bars
     delete h2Cosmics;
   } // Loop over samples
   cout<<nBad<<" bad fits out of "<<nFits<<endl;
   cout<<nStillBad<<" still bad fits (after changing start points) out of "<<nFits<<endl;
-  fout->cd();
-  hGausSigma->Write();
-  hGausMean->Write();
   
   fin->Close();
   delete fin;
