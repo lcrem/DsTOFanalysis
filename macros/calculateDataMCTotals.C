d29c7b1c70208682a8e2a623c4f7dc5d11e17ae7
Seb Jones <jonesseb100@gmail.com>
2021-03-04 13:04:37 +0000

    Update to paper version

--- macros/calculateDataMCTotals.C
+++ macros/calculateDataMCTotals.C
@@ -4,15 +4,18 @@
 // Numbers
 // Data numbers are per spill, 
 const std::vector<double> nMCS4   = {1364, 4113, 4678, 973, 1076}; 
-const std::vector<double> nDataS4 = {122, 185, 167, 49, 5.8};
+const std::vector<double> nDataS4 = {119, 181, 157.9, 38.9, 1.32};
 const std::vector<double> nMCS3   = {49690, 61676, 55944, 17587, 93930};
-const std::vector<double> nDataS3 = {2227, 1881, 1473, 982, 148}; 
+const std::vector<double> nDataS3 = {2441, 2058, 1608, 1069, 166.5}; 
 // Stats errors
-const std::vector<double> nDataErrS4 = {2, 2, 2, 2, 0.2};
-const std::vector<double> nDataErrS3 = {10, 7, 6, 7, 0.5};
+const std::vector<double> nDataErrS4 = {2.1, 2.4, 1.9, 1.4, 0.15};
+const std::vector<double> nDataErrS3 = {10, 8, 7, 8, 0.6};
 // Fractional systematics errors
-const std::vector<double> nMCS4Syst   = {0.095, 0.08, 0.085, 0.171, 0.08};
-const std::vector<double> nDataS4Syst = {0.029, 0.015, 0.067, 0.082, 0.041};
+const std::vector<double> nMCS4Syst      = {0.095, 0.08, 0.085, 0.171, 0.08};
+const std::vector<double> nDataS4AngSyst = {0.029, 0.015, 0.067, 0.082, 0.041};
+const std::vector<double> nDataS4EffSyst = {0.10964, 0.10964, 0.10964, 0.10964, 0.10964};
+const std::vector<double> nDataS4BkgSyst = {0.0018, 0.0016, 0.0114, 0.0141, 0.0811};
+const std::vector<double> nDataS3EffSyst = {0.0113, 0.1143, 0.0698, 0.1143, 0.0492};
 
 void calculateDataMCTotals()
 {
@@ -22,14 +25,16 @@ void calculateDataMCTotals()
 
     double dataRat = nDataS4[b]/nDataS3[b];
     double MCRat   = nMCS4[b]/nMCS3[b];
-    double dataRatFracErr = sqrt( pow(nDataErrS4[b]/nDataS4[b],2) + pow(nDataErrS3[b]/nDataS3[b],2) + nDataS4Syst[b]*nDataS4Syst[b] );
+    double dataRatFracErr = sqrt( pow(nDataErrS4[b]/nDataS4[b],2) + pow(nDataErrS3[b]/nDataS3[b],2) + nDataS4AngSyst[b]*nDataS4AngSyst[b] + nDataS4EffSyst[b]*nDataS4EffSyst[b] + nDataS4BkgSyst[b]*nDataS4BkgSyst[b] + nDataS3EffSyst[b]*nDataS3EffSyst[b] );
     double MCRatFracErr   = sqrt( 1./nMCS4[b] + 1./nMCS3[b] + nMCS4Syst[b]*nMCS4Syst[b] );
 
     double dataMC        = dataRat/MCRat;
     double dataMCFracErr = sqrt(dataRatFracErr*dataRatFracErr + MCRatFracErr*MCRatFracErr);
     
-    std::cout<<"S4/S3: Data = "<<dataRat<<" +- "<<dataRat*dataRatFracErr<<",  MC = "<<MCRat<<" +- "<<MCRat*MCRatFracErr<<std::endl;
+    std::cout<<"S4/S3 MC = "<<MCRat<<" +- "<<MCRat*MCRatFracErr<<", Data = "<<dataRat<<" +- "<<dataRat*dataRatFracErr<<std::endl;
     std::cout<<"Data/MC = "<<dataMC<<" +- "<<dataMC*dataMCFracErr<<std::endl;
+
+    std::cout<<"Data % error = "<<dataRatFracErr*100.<<std::endl;
   }
   
 } // calculateDataMCTotals()
